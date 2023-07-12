57b6d3d166a738d42dbb6ef416a17ef1cc4308c8
YahyaFTW <syedyahya010@gmail.com>
2023-07-12 22:47:36 +0530

    started footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box,Heading,Stack,Container } from '@chakra-ui/react'
+const Footer = () => {
+  return (
+    <>
+      <Box bg={'blackAlpha.900'}>
+         
+      </Box>
+    </>
+  )
+}
+
+export default Footer
