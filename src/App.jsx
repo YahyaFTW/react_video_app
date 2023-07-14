1adc8c8298b8db89ebc53318bf81684e733f77cc
YahyaFTW <syedyahya010@gmail.com>
2023-07-14 22:14:18 +0530

    Laid foundation for videos page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Header from "./components/Header";
-import Home from "./components/Home"
-import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Videoss from './components/Videoss';
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/videos" element={<Videoss />} />
       </Routes>
       <Footer />
     </Router>
